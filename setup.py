89fd72ff4417f921edaf28fc732914c5989c6be3
lixk <lixk@akulaku.com>
2018-10-26 09:34:47 +0800

    update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="py4js",
@@ -11,9 +11,9 @@ setup(
     url="https://github.com/lixk/py4js",
     author="Xiangkui Li",
     author_email="1749498702@qq.com",
-
-    packages=find_packages(),
-    include_package_data=True,
+    py_modules=['py4js'],
+    # packages=find_packages(),
+    # include_package_data=True,
     platforms="any",
     install_requires=["bottle"]
 )
