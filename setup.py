bba3df16e3f338d50471675606a88e52c96121ba
lixk <lixk@akulaku.com>
2018-10-29 11:36:56 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="py4js",
-    version="0.1.1",
+    version="0.1.2",
     keywords=("pip", "py4js"),
     description="a fast and simple micro-framework for small web applications",
     long_description=long_description,
